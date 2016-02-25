ca3bbd7444fb92842b3b4aa599db2fd43b6f61f6
Sergio Oliveira <seocam@seocam.com>
2016-02-25 10:43:38 -0300

    Using dport instead of port
    port is now deprecated

--- manifests/allow_web_from.pp
+++ manifests/allow_web_from.pp
@@ -3,7 +3,7 @@
 define dofirewall::allow_web_from {
   firewall { "110 accept web traffic: $title":
     proto  => 'tcp',
-    port   => [80, 443],
+    dport  => [80, 443],
     action => 'accept',
     source => $title,
   }
